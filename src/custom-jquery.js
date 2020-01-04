d274e263c880beb8c360379ed4fe42484731f54a
Oleg Guchok <oleguchok@gmail.com>
2020-01-04 17:32:39 +0300

    Added removeClass method. Added tests for removeClass and chain

--- src/custom-jquery.js
+++ src/custom-jquery.js
@@ -1,11 +1,20 @@
 export default function $(selector) {
-  const addClass = (className) => {
-    document
-      .querySelector(selector)
+  const addClass = function(className) {
+    _getElement(selector)
       .classList.add(className);
+    return this;
   }
 
+  const removeClass = function(className) {
+    _getElement(selector)
+      .classList.remove(className);
+    return this;
+  }
+
+  const _getElement = (selector) => document.querySelector(selector);
+
   return {
-    addClass
+    addClass,
+    removeClass
   }
 }
--- tests/custom-jquery.test.js
+++ tests/custom-jquery.test.js
@@ -12,4 +12,32 @@ describe('custom-jquery tests', () => {
     const elem = document.querySelector('.base.custom');
     expect(elem).not.toBeNull();
   })
+
+  test('should remove class from element', () => {
+    document.body.innerHTML = /* html */ `
+      <div>
+         <p class="base">Base paragraph</p>
+      </div>`;
+
+    $('.base').removeClass('base');
+
+    const elem = document.querySelector('.base');
+    expect(elem).toBeNull();
+  })
+
+  test('should chain methods', () => {
+    document.body.innerHTML = /* html */ `
+      <div>
+         <p class="base">Base paragraph</p>
+      </div>`;
+
+    $('.base')
+      .addClass('custom')
+      .removeClass('custom');
+
+    const baseElem = document.querySelector('.base');
+    const customElem = document.querySelector('.custom');
+    expect(baseElem).not.toBeNull();
+    expect(customElem).toBeNull();
+  })
 })
